8a35d54fced0c78cdbb3134651214b4be73731ab
BohdanKutsan <K_Bogdan_O@ukr.net>
2021-07-19 15:05:17 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Envirenment} from './interface';
+
+export const environment: Envirenment = {
+  production: true,
+  apiKey: 'AIzaSyDV8AorxJfHZFlM0EhyheqhHi-dzsOqFIk',
+  fbDbUrl: 'https://angular-blog-41508.firebaseio.com'
+};
---
+++ src/environments/interface.ts
@@ -0,0 +1,6 @@
+export  interface  Envirenment {
+  apiKey: string,
+  production: boolean
+  fbDbUrl: string
+
+}
